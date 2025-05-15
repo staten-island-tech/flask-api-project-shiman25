2b7500a3f194475cdb2727bb4c00a011fe63a6d4
shiman25 <shimanz@nycstudents.net>
2025-05-15 08:46:42 -0400

    WHAT?????

--- app.py
+++ app.py
@@ -7,13 +7,5 @@ cards = data.get("cards", [])
 # Get all cards
 cards = Card.all()
 
-# Filter Cards
-# You can chain 'where' clauses together. The key of the hash
-# should be the URL parameter you are trying to filter on
-cards = Card.where(supertypes='legendary') \
-            .where(types='creature') \
-            .where(colors='red,white') \
-            .all()
-
 # Get cards on a specific page / pageSize
 cards = Card.where(page=50).where(pageSize=50).all()
