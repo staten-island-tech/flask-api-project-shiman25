890693906cde3ea4a0422db23db35d6145e95854
shiman25 <shimanz@nycstudents.net>
2025-05-20 09:26:25 -0400

    ?!

--- app.py
+++ app.py
@@ -9,13 +9,29 @@ def index():
     mtg_list = data['cards']
         
     cards = []
-    
+    image = []
     for card in mtg_list:
         cards.append(card)
         #print(card['name'])
-    
+        
         
     return render_template("index.html", cards=cards)
 
 if __name__ == '__main__':
     app.run(debug=True) 
+
+""" from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+
+
+response = requests.get("https://api.magicthegathering.io/v1/cards")
+data = response.json()
+mtg_list = data['cards']
+        
+cards = []
+    
+for card in mtg_list:
+    print(card.get('multiverseid'))
+     """
