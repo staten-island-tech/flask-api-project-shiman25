f2f76b4e5b5119a48d1fe87cfcbe16773d0286ec
shiman25 <shimanz@nycstudents.net>
2025-05-21 09:26:10 -0400

    UGHHHH

--- app.py
+++ app.py
@@ -9,18 +9,30 @@ def index():
     mtg_list = data['cards']
         
     cards = []
-    image = []
+    images = []
     for card in mtg_list:
-        cards.append(card)
-        #print(card['name'])
+        cards.append(card['name'])
+        images.append(card.get("imageUrl"))
         
-        
+        cards.append({
+            'name': card['name'],
+            'id': card.get('multiverseid'),
+            'image': card.get('imageUrl')
+        })
+
     return render_template("index.html", cards=cards)
+
+@app.route("/detail")
+def detail():
+    return render_template("detail.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
-
-""" from flask import Flask, render_template
+""" 
+from flask import Flask, render_template
 import requests
 app = Flask(__name__)
 
@@ -31,7 +43,12 @@ data = response.json()
 mtg_list = data['cards']
         
 cards = []
-    
+image=[]
 for card in mtg_list:
-    print(card.get('multiverseid'))
-     """
+        cards.append(card)
+        image.append(card.get('imageUrl'))
+
+print(image)
+        #print(card['name'])     """
+
+    
